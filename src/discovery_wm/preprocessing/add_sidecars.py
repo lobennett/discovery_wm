4bfa883750e0e34e053028f378528af75cd823ba
Logan Bennett <bennett.j.logan@gmail.com>
2025-04-02 17:18:43 -0700

    add fmap source to bold, remove .json

--- src/discovery_wm/preprocessing/add_sidecars.py
+++ src/discovery_wm/preprocessing/add_sidecars.py
@@ -12,7 +12,7 @@ def create_json_path(subj, ses):
 def create_json_data(subj, ses):
     """Create a json fmap data for a subject and session."""
     subj_id = subj.name
-    basename = f'{subj_id}_{ses.name}_run-1_fieldmap.json'
+    basename = f'{subj_id}_{ses.name}_run-1_fieldmap'
     return {
         "B0FieldIdentifier": basename
     }
@@ -29,18 +29,26 @@ def main():
         sessions = get_subj_sessions(subj)
         for ses in sessions:
             fmap_dir = ses / "fmap"
+            func_dir = ses / "func"
+
             if not fmap_dir.exists():
                 raise ValueError(f"No fmap directory found for {subj} {ses}")
             
             json_fmap = create_json_path(subj, ses)
             json_data = create_json_data(subj, ses)
+            B0FieldSource = json_data["B0FieldIdentifier"]
 
-            if not json_fmap.exists():
-                logging.info(f"Added json fmap file for {ses}")
-                dump_json(json_data, json_fmap)
-            else:
-                logging.warning(f"Skipping {subj} {ses}: already exists")
-    
+            logging.info(f"Added json fmap file for {ses}")
+            dump_json(json_data, json_fmap)
+
+            func_files = list(func_dir.glob("*.json"))
+            for func_file in func_files:
+                # Change B0FieldSource to B0FieldSource
+                with open(func_file, 'r') as f:
+                    data = json.load(f)
+                data["B0FieldSource"] = B0FieldSource
+                with open(func_file, 'w') as f:
+                    json.dump(data, f)
 
 if __name__ == "__main__":
     main()
